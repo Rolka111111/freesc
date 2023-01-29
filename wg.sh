1ef6cdb8720610610ba8e7f38f19e8a53d7ebbf8
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-01-29 18:56:32 +0200

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -89,10 +89,10 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O add-wg "https://raw.githubusercontent.com/senowahyu62/freesc/main/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/senowahyu62/freesc/main/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/senowahyu62/freesc/main/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/senowahyu62/freesc/main/renew-wg.sh"
+wget -O add-wg "https://raw.githubusercontent.com/Rolka111111/freesc/main/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/Rolka111111/freesc/main/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/Rolka111111/freesc/main/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/Rolka111111/freesc/main/renew-wg.sh"
 chmod +x add-wg
 chmod +x del-wg
 chmod +x cek-wg
