71eac1c2daeff77f8450dee6bb1c319b93a7ee3b
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-01 22:02:02 +0200

    Update menu.sh

--- menu-all/menu.sh
+++ menu-all/menu.sh
@@ -64,7 +64,7 @@ echo -e   "  \e[1;32m-----------------------------------------------------------
 echo -e   "   x)   Exit" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-8 or x] :  " menu
+read -p "     Select From Options [1-27 or x] :  " menu
 echo -e   ""
 case $menu in
 1)
